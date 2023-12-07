9d5e07e3540f0f8768b36fb9634be06dc9812729
whoyoshi <diana.azamatovva@gmail.com>
2023-12-07 19:26:57 +0300

    добавили картинки и написали код для маршрута, который отображает страницу перс-й

--- bleach/routes/index.js
+++ bleach/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* Страница Куросаки */
+router.get('/kurosaki', function(req, res, next) {
+  res.send("<h1>Страница Куросаки Ичиго</h1>")
+});
+
+/* Страница Рукии */
+router.get('/rukia', function(req, res, next) {
+  res.send("<h1>Страница Кучики Рукии</h1>")
+});
+
+/* Страница Уноханы */
+router.get('/unohana', function(req, res, next) {
+  res.send("<h1>Страница Уноханы Рецу</h1>")
+});
+
+
 module.exports = router;
