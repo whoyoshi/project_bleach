b2b544540cbd7b427b22824b55d3568544660456
whoyoshi <diana.azamatovva@gmail.com>
2023-12-09 20:09:28 +0300

    5.3 переход между страницами

--- bleach/routes/index.js
+++ bleach/routes/index.js
@@ -6,6 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница главная */
+router.get('/main', function(req, res, next) {
+  res.render('bleach', {
+    title: "Bleach",
+    picture:"images/bleach.png",
+    desc: "«Bleach» - это манга Тайто Кубо и её адаптация. Манга была опубликована с августа 2001 по август 2016 года в журнале Shonen Jump. В настоящее время манга завершена и состоит из 686 глав, объединенных в 74 тома. В 2005 году манга «Bleach» была удостоена премии издательства Shogakukan как лучшая манга в категории сёнен (для мальчиков). Она также включена в список самых продаваемых комиксов в Японии и США."
+  });
+});
 
 /* Страница Куросаки */
 router.get('/kurosaki', function(req, res, next) {
