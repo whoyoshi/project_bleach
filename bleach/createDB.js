d30a211b747892df59ecd435c15085bb834a0ba1
whoyoshi <diana.azamatovva@gmail.com>
2023-12-09 21:29:42 +0300

    6.1 подключение бд

---
+++ bleach/createDB.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+  try {
+    await client.connect();
+    var database = client.db("bleachDB");
+    database.dropDatabase()
+    database = client.db("bleachDB");
+    const bleaches = database.collection("bleaches");
+    const result = await bleaches.insertOne({name:"Блич"});
+    console.log(`${result} documents were inserted`);
+  } finally {
+    await client.close();
+  }
+}
+run()
