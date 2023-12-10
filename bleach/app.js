05ccf8e45e472ca96eb489e6b12d649406a27d4a
whoyoshi <diana.azamatovva@gmail.com>
2023-12-10 21:03:57 +0300

    8.2 обработка параметра в адресе

--- bleach/app.js
+++ bleach/app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost/bleachDB')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var shinigami = require('./routes/shinigami');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/shinigami', shinigami);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
