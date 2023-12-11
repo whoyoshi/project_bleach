63e2410c191ecdd2ec0cf2802465a17f9d799887
whoyoshi <diana.azamatovva@gmail.com>
2023-12-11 15:18:22 +0300

    8.2 обработка параметра в адресе

--- bleach/routes/shinigami.js
+++ bleach/routes/shinigami.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 /* Страница шинигами */
 router.get("/:nick", function(req, res, next) {
-res.send(req.params.nick);
+  res.send(req.params.nick);
 });
 
 module.exports = router;
