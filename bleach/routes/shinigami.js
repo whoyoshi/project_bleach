ab5309014b494eab1b61fef9696faee63be9e4a2
whoyoshi <diana.azamatovva@gmail.com>
2023-12-11 14:59:20 +0300

    8.3 извлечение данных из базы

--- bleach/routes/shinigami.js
+++ bleach/routes/shinigami.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Bleach = require("C:/aip/project_bleach/bleach/models/bleach.js").Bleach;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с shinigami');
 });
 
-/* Страница шинигами */
-router.get("/:nick", function(req, res, next) {
-res.send(req.params.nick);
+// /* Страница шинигами */
+router.get("/:nick", async (req, res, next) => {
+  try {
+    const bleach = await Bleach.findOne({ nick: req.params.nick });
+    console.log(bleach);
+    if (!bleach) {              //код проверяет наличие объекта с заданным "nick" в базе данных и обрабатывает результаты запроса.
+      throw new Error("Нет такого Шинигами!");
+    }
+    res.render('bleach', {
+      title: bleach.title,
+      picture: bleach.avatar,
+      desc: bleach.desc
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
