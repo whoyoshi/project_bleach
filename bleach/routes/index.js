a12c578111d6dccd09c44dc132a051ea4769331d
whoyoshi <diana.azamatovva@gmail.com>
2024-01-08 23:50:32 +0300

    10.1 создание страницы регистрации

--- bleach/routes/index.js
+++ bleach/routes/index.js
@@ -4,31 +4,21 @@ const Bleach = require("../models/bleach").Bleach;
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Loq in'});
+  res.render('logreg',{title: 'Log In'});
 });
 
-/* Страница главная */
+/* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-    
-    const mainPageData = {
-      title: 'Bleach',
-      picture: "/images/bleach.png",
-      desc: "«Bleach» - это манга Тайто Кубо и её аниме адаптация. Манга была опубликована с августа 2001 по август 2016 года в журнале Shonen Jump. В настоящее время манга завершена и состоит из 686 глав, объединенных в 74 тома. В 2005 году манга «Bleach» была удостоена премии издательства Shogakukan как лучшая манга в категории сёнен. Она также включена в список самых продаваемых комиксов в Японии и США.",
-      counter:req.session.counter,
-    };
-
-    req.session.greeting = "Hi!!!";
-    // Запрос данных из базы данных 
-    mainPageData.menu = await Bleach.find({}, { _id: 0, title: 1, nick: 1 });
-
-    //  отвечает за рендеринг страницы с использованием шаблона bleach.ejs.
-    res.render('bleach', mainPageData);
-  } catch (err) {
+    const menu = await Bleach.find({}, { _id: 0, title: 1, nick: 1 });
+    req.session.greeting = "Hi!!!"
+    res.render('index', { title: 'Bleach', menu: menu, counter:req.session.counter });
+  } 
+  catch (err) {
     next(err);
   }
 });
-
+   
 module.exports = router;
 
 
