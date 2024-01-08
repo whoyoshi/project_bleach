989ed86896eabe8bb55e86b07e54ccf70c6cbb08
whoyoshi <diana.azamatovva@gmail.com>
2024-01-09 02:22:28 +0300

    10.4 логика пользователя

--- bleach/models/user.js
+++ bleach/models/user.js
@@ -1,33 +1,44 @@
 var mongoose = require("mongoose");
 var crypto = require("crypto");
 
-var schemaUser = mongoose.Schema({ 
-    username:{
-      type: String,
-      unique: true,
-      required: true
-    }, 
-    hashedPassword:{
-      type: String,
-      required: true },
-    salt:{
-      type: String,
-      required: true },
-    created:{ 
-      type:Date,
-      default: Date.now 
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-schemaUser.virtual("password").set(function(password){ this._purePassword = password
-  this.salt = Math.random() + ""
-  this.hashedPassword = this.encryptPassword(password)
-}).get(function(){
-    return this._purePassword
+userSchema.virtual("password")
+.set(function(password) {
+  this._purePassword = password;
+  this.salt = Math.random() + "";
+  this.hashedPassword = this.encryptPassword(password);
+})
+.get(function() {
+  return this._purePassword;
 });
-schemaUser.methods.encryptPassword = function(password){ 
-  return crypto.createHmac('sha1', this.salt).update(password).digest('hex') 
+
+userSchema.methods.encryptPassword = function(password) {
+return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
 };
-  
-module.exports.User = mongoose.model("User",schemaUser)
+
+userSchema.methods.checkPassword = function(password){
+return this.encryptPassword(password) === this.hashedPassword
+}
+
+module.exports.User = mongoose.model("User", userSchema);
+
     
--- bleach/routes/index.js
+++ bleach/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Bleach = require("../models/bleach").Bleach;
+const User = require("../models/user").User;
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
@@ -20,10 +21,33 @@ router.get('/', async (req, res, next) => {
 });
 
 /* POST login/registration page. */
-router.post('/logreg', function(req, res, next) { 
-  var username = req.body.username 
-  var password = req.body.password
+router.post('/logreg', async function(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+      const user = await User.findOne({ username });
+
+      if (user) {
+          if (user.checkPassword(password)) {
+              req.session.user = user._id;
+              res.redirect('/');
+          } else {
+              res.render('logreg', { title: 'Log In', error: 'Неверный пароль' });
+          }
+      } else {
+          const newUser = new User({ username, password });
+          await newUser.save();
+          req.session.user = newUser._id;
+          res.redirect('/');
+      }
+  } catch (err) {
+      next(err);
+  }
 });
+
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{error:null});
+  });
    
 module.exports = router;
 
